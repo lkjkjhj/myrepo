e284f40eccbb9e8dec13fe7fe9574edfda3da8a7
lkjkjhj <dnhw7512@naver.com>
2021-10-26 23:09:38 +0900

    2 33333 4

--- 01_hello_git/src/com/kh/git/HelloGit.java
+++ 01_hello_git/src/com/kh/git/HelloGit.java
@@ -10,9 +10,8 @@ public class HelloGit {
 	}
 	
 	public void merge() {
-		System.out.println(1);
 		System.out.println(2);
-		System.out.println(3);
+		System.out.println(33333);
 		System.out.println(4);
 	}
 
